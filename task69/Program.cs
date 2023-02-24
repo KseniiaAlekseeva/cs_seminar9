eb7f32a8fffeedd0e436068583ef1b396fb24f99
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-24 16:39:02 +0300

    Add task69

---
+++ task69/Program.cs
@@ -0,0 +1,16 @@
+﻿// Program, which raises number A to the power B by recursion.
+
+int Pow(int a, int b)
+{
+    if (b == 1)
+        return a;
+    else
+        return a * Pow(a, b - 1);
+}
+
+Console.WriteLine("Enter the foundation A in A^B: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the degree B in A^B: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{a}^{b} = {Pow(a, b)}.");
