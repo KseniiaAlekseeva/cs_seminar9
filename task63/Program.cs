70309673a437b7c6c59b3a8e499bdcbf942d2e6d
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-24 15:31:35 +0300

    Add task63

---
+++ task63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Program output all natural numbers from 1 to N
+
+void PrintNumbers(int n, int start)
+{
+    Console.Write(start + ", ");
+    if (start == n)
+        return;
+    else
+        PrintNumbers(n, start + 1);
+}
+
+Console.WriteLine("Enter the natural number: ");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumbers(n, 1);
