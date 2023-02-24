0028a8d442c9ae04bdd509f863afc3b752e1f856
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-24 15:43:59 +0300

    Add task65

---
+++ task65/Program.cs
@@ -0,0 +1,17 @@
+﻿// Enter two natural numbers M and N. Write program, which outputs all natural numbers in range [M,N]
+
+string NumbersInRange(int m, int n)
+{
+    string str = Convert.ToString(m);
+    if (m == n)
+        return str + ".";
+    else
+        return str + ", " + Convert.ToString(NumbersInRange(m + 1, n));
+}
+
+Console.WriteLine("Enter the first natural number M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second natural number N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(NumbersInRange(m, n));
