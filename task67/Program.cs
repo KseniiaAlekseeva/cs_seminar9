3bee682d6c8686606683b536901a7aaa52ed742e
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-24 16:00:57 +0300

    Add task67

---
+++ task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Program outputs the sum of digits of entered integer number.
+
+int SumDigits(int n)
+{
+    if (n / 10 == 0)
+        return n;
+    else
+        return n % 10 + SumDigits(n / 10);
+}
+
+Console.WriteLine("Enter the first natural number M: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The sum of digits of number {n} is {SumDigits(n)}.");
